03e3b2996bc21835111662df2ff9b1447b1a1871
Yanmife123 <yanmifeigwe@gmail.com>
2024-03-31 20:28:03 +0100

    Helping a UI/UX to create a website

---
+++ script.js
@@ -0,0 +1,6 @@
+function myfunction1() {
+  document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
+}
+function myfunction2() {
+  document.getElementById("section3").scrollIntoView({behavior:"smooth"});
+}
